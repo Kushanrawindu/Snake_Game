e60d42d9b6ed62412a8eb3e76e5bc8c1334433cb
Kushanrawindu <kushanrawindu@gmail.com>
2020-08-24 12:30:20 +0530

    visualize the snake element

--- snake.js
+++ snake.js
@@ -1,9 +1,17 @@
 export const SNAKE_SPEED =2
+const snakeBody = [{ x:11, y:11}]   //set the snake position 
 
 export function update(){
-
+    
 }
 
-export function draw(){
-
+export function draw(gameBoard){
+    snakeBody.forEach(segment =>{
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.x
+        snakeElement.style.gridColumnStart = segment.y 
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement) 
+    })
+    
 }
