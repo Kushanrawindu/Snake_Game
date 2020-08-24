bdd515053d34cb83750858993d6c305a5e6081ae
Kushanrawindu <kushanrawindu@gmail.com>
2020-08-24 13:50:49 +0530

    let snake to eat food

--- food.js
+++ food.js
@@ -1,7 +1,14 @@
-let food = {x:0, y:0}
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+let food = getRandomFoodPosition()
+const EXPANSION_RATE =1
 
 export function update(){
-    
+    if(onSnake(food)){
+        expandSnake(EXPANSION_RATE)
+        food = getRandomFoodPosition()
+    }
 }
 
 export function draw(gameBoard){
@@ -11,4 +18,12 @@ export function draw(gameBoard){
         foodElement.classList.add('food')
         gameBoard.appendChild(foodElement)
     
+}
+
+function getRandomFoodPosition(){
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)){
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
 }
