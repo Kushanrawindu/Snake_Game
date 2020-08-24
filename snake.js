7f041420380a9ac2ddb0bceb9f39b2765b4f2e8a
Kushanrawindu <kushanrawindu@gmail.com>
2020-08-24 13:17:53 +0530

    add the food item

--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 import { getInputDirection } from "./input.js"
 
-export const SNAKE_SPEED =2
+export const SNAKE_SPEED =4
 const snakeBody = [{ x:10, y:11}]   //set the snake position 
 
 export function update(){
